4352e056e395f58cd6ae35e71a9c71019613c049
IBBear <jgilbert868@gmail.com>
2021-06-24 07:52:55 -0700

    created prompt to ask for password lenght.

--- assets/javaScript/passwordGeneratorScript.js
+++ assets/javaScript/passwordGeneratorScript.js
@@ -1,10 +1,15 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+   //Ask user for input on lenght
+   var userInput = prompt("Enter a number between 8 and 128. This will be the password lenght");
+   console.log(userInput);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+ 
 
   passwordText.value = password;
 
@@ -12,3 +17,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
