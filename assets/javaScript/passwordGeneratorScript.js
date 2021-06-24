76c382402a01cb7cfea4209dd414defd8dcab3d2
IBBear <jgilbert868@gmail.com>
2021-06-24 09:21:40 -0700

    created function for user input

--- assets/javaScript/passwordGeneratorScript.js
+++ assets/javaScript/passwordGeneratorScript.js
@@ -1,20 +1,19 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
+var userInput;
+function generatePassword(){
    //Ask user for input on lenght
-   var userInput = prompt("Enter a number between 8 and 128. This will be the password lenght");
-   console.log(userInput);
+   userInput = prompt("Enter a number between 8 and 128. This will be the password lenght");  
+
+};
+// Write password to the #password input
+function writePassword() {   
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
- 
-
   passwordText.value = password;
-
-}
-
+} 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
