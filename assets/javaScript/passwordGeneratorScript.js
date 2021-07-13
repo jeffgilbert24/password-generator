5b05a660f71886d0c7ba5ef975b7b608225faf54
IBBear <jgilbert868@gmail.com>
2021-07-13 09:52:08 -0700

    changed js file to include better formate

--- assets/javaScript/passwordGeneratorScript.js
+++ assets/javaScript/passwordGeneratorScript.js
@@ -1,32 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// store user answerS
-var x; 
-var numlet;
-var generatePassword= [];
-var symb;
-
+var lowChar = "abcdefghijklmnopqrstuvwxyz";
+var upLowChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var smallSink = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+var kitchenSink = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&^?"
 
 // Write password to the #password input
 function writePassword() {
-  var userInput = prompt("Define the lenght of the password");
-  x = prompt("you have entered "+ userInput +" Press button");
-  userInput = prompt("Will this password contain both letters and numbers ? enter yes or no");
-  numlet = prompt("you have entered "+ userInput + " Press button"); 
-  userInput = prompt("Will this password contain special charaters ? enter yes or no");
-  symb = prompt("you have entered "+ userInput +" press button");
-  // Generate number password
-var randomNum = Math.floor((Math.random() * x) +1);
-generatePassword = document.getElementsByName("x");
-generatePassword = prompt(randomNum);
+  var x = prompt("choose a number between 4 and 20, this will be the lenght of the password.")
 
+  if(x < 4 || x >20){
+    alert("pic a different number")
+   }else{
+     console.log(x);
+   }
 
+   
+    
+    var password = prompt("Your password contains lower case letters, if you want upper and lower click OK!");
+    
+    if (password !== true) {
+      console.log("your password is upper and lower");
+    } else {
+      console.log("Lower it is!");
+      
+    }
+    
+
+ // var passwordText = document.querySelector("#password");
+  
+ // passwordText.value = password;
 
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 
