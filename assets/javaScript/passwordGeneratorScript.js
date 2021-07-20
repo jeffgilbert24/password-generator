ed25ccad4a6b78c5764141e06e049638120e1615
IBBear <jgilbert868@gmail.com>
2021-07-20 09:27:12 -0700

    can not shorten the string

--- assets/javaScript/passwordGeneratorScript.js
+++ assets/javaScript/passwordGeneratorScript.js
@@ -1,11 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowChar = "abcdefghijklmnopqrstuvwxyz";
-//var lowChar = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
 var upLowChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 var smallSink = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 var kitchenSink = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&^?";
+var lenght;
 var x;
+var pw;
 
 // changing string to random 
 String.prototype.shuffle = function () {
@@ -21,35 +22,39 @@ String.prototype.shuffle = function () {
   
 }
 
-console.log(lowChar.shuffle());
 
 
 
 // ask the user to pick the size of the password
 function writePassword() {
-  x = prompt("choose a number between 4 and 20, this will be the lenght of the password.")
-
-  if(x < 4 || x >20){
+   lenght = prompt("choose a number between 4 and 20, this will be the lenght of the password.");
+  if(lenght < 4 || lenght >20){
     alert("pic a different number")
    }else{
-     x = prompt("You have chosen "+ x +" click ok button to continue.");
-     console.log(x);          
-   }  
-var number = x;
+     shorten = prompt("You have chosen "+ lenght +" click ok button to continue.");
+     console.log(lenght);          
+   } 
+   
+
 // ask the user the makeup of the password.     
-    var shorten;
-    var password = prompt("Your password contains lower case letters, if you want upper and lower click OK!");    
-    if (password === true) {
-      console.log("your password is upper and lower");      
-    } else {    
+    x ===parseInt(length);
+    var password = prompt("Your password contains lower case letters, if you want upper and lower click OK!");
+    // return password   
+    if (password !== true) {
       password = lowChar.shuffle();
-     shorten = password.substring(0, 8);  
-      console.log(shorten);
-      console.log("Lower it is!");
-    }
-     
-      
-    }   
+      console.log(password.substring(0, x)); 
+document.getElementById("password").innerHTML = password.substring(0, x);  
+// provide a second choice 
+} else {    
+var secPass = prompt("Your password contains upper and lower characters, do you want numbers? click ok for yes.");
+// return password
+if (secPass !==true){
+  secPass = upLowChar.shuffle();
+  console.log(secPass.substring(0, 8));
+document.getElementById("password").innerHTML = secPass.substring(0, x);
+}
+}
+}
     
     
 
